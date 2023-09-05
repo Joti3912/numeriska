846218247afd3ba38b5359f61eda5769de7a7ee2
Kalle <kallenordgren55@gmail.com>
2023-09-05 11:45:15 +0200

    added dp1 and dp2

--- proj1.py
+++ proj1.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy as sp
+from scipy.integrate import solve_ivp
 
 m = 1
 l = 1 
@@ -10,14 +10,26 @@ ml2 = m*l*l
 #p1 = 0
 #p2 = 0
 
-def dtheta1(t, theta1, theta2, p1, p2):
+def dtheta1(theta1, theta2, p1, p2):
     taljare = 2*p1 - 3*np.cos(theta1 - theta2)*p2
     namnare = 16 - 9*(np.cos(theta1 - theta2))^2
     return (6/ml2)*(taljare /  namnare) 
 
-def dtheta2(t, theta1, theta2, p1, p2):
+def dtheta2(theta1, theta2, p1, p2):
     taljare = 8*p2 - 3*np.cos(theta1 - theta2)*p1
     namnare = 16 - 9*(np.cos(theta1 - theta2))^2
-    return (6/ml2)*(taljare /  namnare) 
+    return (6/ml2)*(taljare / namnare) 
+
+def dp1(theta1, theta2, p1, p2):
+    dtheta1 = dtheta1(theta1, theta2, p1, p2)
+    dtheta2 = dtheta2(theta1, theta2, p1, p2)
+    negml2 = (-(1/2))*ml2
+    return negml2*(dtheta1*dtheta2*np.sin(theta1 - theta2) + 3*(g/l)*np.sin(theta1))
+
+def dp2(theta1, theta2, p1, p2):
+    dtheta1 = dtheta1(theta1, theta2, p1, p2)
+    dtheta2 = dtheta2(theta1, theta2, p1, p2)
+    negml2 = (-(1/2))*ml2
+    return negml2*(-dtheta1*dtheta2*np.sin(theta1 - theta2) + 3*(g/l)*np.sin(theta2))
 
 #skriv delta 
