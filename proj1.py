697bd63f754aa9a94fbb4bfcfd3452befcdfe776
Joti3912 <josef.tiljander@gmail.com>
2023-09-07 12:36:42 +0200

    added euler method. Doesn't print any values past time t = ~0.5

--- proj1.py
+++ proj1.py
@@ -33,16 +33,16 @@ def dp2(theta1, theta2, p1, p2):
     negml2 = (-(1/2))*ml2
     return negml2*(-x*y*np.sin(theta1 - theta2) + (g/l)*np.sin(theta2))
 
-def f(t, y):
+def ode(t, y):
     theta1, theta2, p1, p2 = y
     dtheta1_dt = dtheta1(theta1, theta2, p1, p2)
     dtheta2_dt = dtheta2(theta1, theta2, p1, p2)
     dp1_dt = dp1(theta1, theta2, p1, p2)
     dp2_dt = dp2(theta1, theta2, p1, p2)
-    return [dtheta1_dt, dtheta2_dt, dp1_dt, dp2_dt]
+    return np.array([dtheta1_dt, dtheta2_dt, dp1_dt, dp2_dt])
 
     
-result = solve_ivp(f, (0,10), (np.pi/10, np.pi/10, 0, 0))
+result = solve_ivp(ode, (0,10), (np.pi/10, np.pi/10, 0, 0))
 print(result)
 plt.plot(result.t, result.y[0], label="theta1(t)")
 plt.plot(result.t, result.y[1], label="theta2(t)")
@@ -50,3 +50,24 @@ plt.xlabel('t')
 plt.ylabel('y')
 plt.legend()
 plt.show()
+
+def euler(f, tspan, u0, dt):
+    freq = round((tspan)/dt)            #antalet tidpunkter
+    tvec = np.linspace(tspan, freq+1)   #array med alla tidpunkter
+    u = np.zeros((len(tvec),len(u0)))   #tidpunkter x variabler
+    i = 0
+    u[i, :] = u0                        #set start values   
+    for t in tvec[0:len(tvec)-1]:
+        u[i+1,:] = u[i,:]+dt*f(t,u[i,:])
+        i += 1
+    return u  
+
+abcd = euler(ode, 10, [np.pi/10, np.pi/10, 0, 0], 0.01)
+print(abcd)
+#        y1  y2  y3
+#    _____________
+#    t1|
+#    t2|
+#    .
+#    .
+#    .
