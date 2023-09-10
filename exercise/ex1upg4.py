6fd0bd42711c5c67b9c97b25cb389213739ce58a
Joti3912 <josef.tiljander@gmail.com>
2023-09-10 20:01:34 +0200

    upg4

--- exercise/ex1upg4.py
+++ exercise/ex1upg4.py
@@ -1,28 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
-
+from scipy.optimize import fsolve
 
 def rhsODE(t,y): 
     return np.sin(t) - y
 
-#Heuns method
+#Euler Implicit
 def solve(fun, tstop, y0, h):
     steps = round(tstop/h)
     tvec = np.linspace(0, tstop, steps)
     i = 0
-    y = np.zeros((len(tvec), 1))
+    y = np.zeros((len(tvec)))
     y[i] = y0
-    for t in tvec[0:len(tvec)-1]: #OBS! kan ej använda range då rhsODE beror på t
-        k1 = fun(t,y[i])
-        k2 = fun(t+h, y[i] + h*k1)
-        y[i+1] = y[i] + (h/2)*(k1 + k2)
+    for t in tvec[0:len(tvec)-1]: 
+        g = lambda y_new: y_new - y[i] - h*fun(t+h, y_new)
+        y[i+1] = fsolve(g, y[i])
         i += 1
     return tvec, y
 
 
-ts, y = solve(rhsODE, 10.0, [0], 0.01)
+ts, y = solve(rhsODE, 10.0, 0, 0.01)
 plt.plot(ts, y, label="y(t)")
+plt.title('Uppgift 4, Implicit Euler')
 plt.xlabel('t')
 plt.xlabel('y')
 plt.legend()
